20940e2c599e1890fe2c856ede31975fc13723c2
Dibayendu Dey <dibayendudey@Lens10s-Spare-5.local>
2014-10-08 11:26:35 +1100

    starting the resque worker and resque scheduler

--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -10,10 +10,10 @@ if ['solo', 'util'].include?(node[:instance_role])
   end
 
   case node[:ec2][:instance_type]
-  when 'm1.small' then worker_count = 2
+  when 'm1.small' then worker_count = 3
   when 'c1.medium'then worker_count = 3
-  when 'c1.xlarge' then worker_count = 8
-  else worker_count = 4
+  when 'c1.xlarge' then worker_count = 3
+  else worker_count = 3
   end
 
 
