f1a3fcaacd0c52d65d1c2f6993440962436ce415
Dibayendu Dey <dibayendudey@Lens10s-Spare-5.local>
2014-11-12 12:18:22 +1100

    production server is now set to run two resque workers

--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -10,10 +10,10 @@ if ['solo', 'util'].include?(node[:instance_role])
   end
 
   case node[:ec2][:instance_type]
-  when 'm1.small' then worker_count = 1
-  when 'c1.medium'then worker_count = 1
-  when 'c1.xlarge' then worker_count = 1
-  else worker_count = 1
+  when 'm1.small' then worker_count = 2
+  when 'c1.medium'then worker_count = 2
+  when 'c1.xlarge' then worker_count = 2
+  else worker_count = 2
   end
 
 
