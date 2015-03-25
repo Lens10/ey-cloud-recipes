bd6ca871f47ff2cf1998addb863961da86269ce8
Dibayendu Dey <dibayendudey@Lens10s-Spare-5.local>
2015-03-25 18:02:46 +1100

    looks like we have to mention a version number
    Chef::Exceptions::Package
    -------------------------
    No version specified, and no candidate version available for www-client/google-chrome
    
    Resource Declaration:
    ---------------------
    # In /etc/chef-custom/recipes/cookbooks/chrome/recipes/default.rb
    
      7:   package "www-client/google-chrome"
      8: end
    
    Compiled Resource:
    ------------------
    # Declared in /etc/chef-custom/recipes/cookbooks/chrome/recipes/default.rb:7:in `from_file'
    
    package("www-client/google-chrome") do
      action :install
      retries 0
      retry_delay 2
      package_name "www-client/google-chrome"
      cookbook_name :chrome
      recipe_name "default"
    end

--- cookbooks/chrome/recipes/default.rb
+++ cookbooks/chrome/recipes/default.rb
@@ -4,6 +4,8 @@
 #
 
 if ['solo', 'util'].include?(node[:instance_role])
-  package "www-client/google-chrome"
+  package "www-client/google-chrome" do
+    version node[:chrome][:version]
+  end
 end
 
