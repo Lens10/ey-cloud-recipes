eacbe9de0c4677585f578f5d911b85b7f255ab22
Dibayendu Dey <dibayendudey@Lens10s-Spare-5.local>
2015-03-25 17:55:29 +1100

    gentoo did not like having a version number too
    added `:stable` to download the stable most google-chrome version
    
    Chef::Exceptions::Exec
    ----------------------
    emerge -g --color n --nospinner --quiet =www-client/google-chrome-41.0.2272.101_p1 returned 1, expected 0

--- cookbooks/chrome/recipes/default.rb
+++ cookbooks/chrome/recipes/default.rb
@@ -4,8 +4,6 @@
 #
 
 if ['solo', 'util'].include?(node[:instance_role])
-  package "www-client/google-chrome" do
-    version node[:chrome][:version]
-  end
+  package "www-client/google-chrome:stable"
 end
 
