9d6bea0da3c8899ab6667757aae25d2192cb029f
Dibayendu Dey <dibayendudey@Lens10s-Spare-5.local>
2015-03-26 11:22:33 +1100

    looked into cookbook/packages to figure out how to install google-chrome
    using override_hardmask flag to true which uses —-autounmask-write

--- cookbooks/chrome/recipes/default.rb
+++ cookbooks/chrome/recipes/default.rb
@@ -4,8 +4,18 @@
 #
 
 if ['solo', 'util'].include?(node[:instance_role])
+  ey_cloud_report "google chrome install" do
+    message "Installing google-chrome-#{node[:chrome][:version]}"
+  end
+
+  enable_package "www-client/google-chrome" do
+    version node[:chrome][:version]
+    override_hardmask true
+  end
+
   package "www-client/google-chrome" do
     version node[:chrome][:version]
+    action :install
   end
 end
 
